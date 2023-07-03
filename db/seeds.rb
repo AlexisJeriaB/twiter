e6b98b3cee62d1f2b113089eaaaa89fc114ea6a9
ale_ubuntu <alexisjebe@gmail.com>
2023-07-03 13:12:19 -0400

    segundo commit, agregando seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do |i|
+    Tweet.create(  description: "descripcion #{i + 1}",
+                    username: Faker::Twitter.screen_name)
+end
